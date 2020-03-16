7000a21f17e825cc6001f94b765a13f833f0d219
dherrada <dylan.herrada@gmail.com>
2020-03-16 14:48:58 -0400

    Ran black, updated to pylint 2.x

--- examples/bmp280_normal_mode.py
+++ examples/bmp280_normal_mode.py
@@ -14,9 +14,9 @@ i2c = busio.I2C(board.SCL, board.SDA)
 bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
 
 # OR create library object using our Bus SPI port
-#spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
-#bmp_cs = digitalio.DigitalInOut(board.D10)
-#bmp280 = adafruit_bmp280.Adafruit_BMP280_SPI(spi, bmp_cs)
+# spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+# bmp_cs = digitalio.DigitalInOut(board.D10)
+# bmp280 = adafruit_bmp280.Adafruit_BMP280_SPI(spi, bmp_cs)
 
 # change this to match the location's pressure (hPa) at sea level
 bmp280.sea_level_pressure = 1013.25
@@ -25,7 +25,7 @@ bmp280.standby_period = adafruit_bmp280.STANDBY_TC_500
 bmp280.iir_filter = adafruit_bmp280.IIR_FILTER_X16
 bmp280.overscan_pressure = adafruit_bmp280.OVERSCAN_X16
 bmp280.overscan_temperature = adafruit_bmp280.OVERSCAN_X2
-#The sensor will need a moment to gather inital readings
+# The sensor will need a moment to gather inital readings
 time.sleep(1)
 
 while True:
