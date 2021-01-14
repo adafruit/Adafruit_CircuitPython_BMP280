8ba22c1f4dc9effff6c5d04ebff195f6044aa1f3
dherrada <dylan.herrada@adafruit.com>
2021-01-14 11:25:15 -0500

    Ran pre-commit, added licenses

--- examples/bmp280_simpletest.py
+++ examples/bmp280_simpletest.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
 """Simpletest Example that shows how to get temperature,
    pressure, and altitude readings from a BMP280"""
 import time
