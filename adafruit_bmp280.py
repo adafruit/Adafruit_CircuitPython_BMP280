0341f0895430569d57bc360de1f924465588a663
jposada202020 <jquintana202020@gmail.com>
2021-04-23 12:24:26 -0400

    Removing pressure limitations, to unify behavior to BME280

--- adafruit_bmp280.py
+++ adafruit_bmp280.py
@@ -35,9 +35,6 @@ _REGISTER_CONFIG = const(0xF5)
 _REGISTER_PRESSUREDATA = const(0xF7)
 _REGISTER_TEMPDATA = const(0xFA)
 
-_BMP280_PRESSURE_MIN_HPA = const(300)
-_BMP280_PRESSURE_MAX_HPA = const(1100)
-
 
 """iir_filter values"""
 IIR_FILTER_DISABLE = const(0)
@@ -320,18 +317,17 @@ class Adafruit_BMP280:  # pylint: disable=invalid-name
         var3 = self._pressure_calib[2] * var1 * var1 / 524288.0
         var1 = (var3 + self._pressure_calib[1] * var1) / 524288.0
         var1 = (1.0 + var1 / 32768.0) * self._pressure_calib[0]
-        if not var1:
-            return _BMP280_PRESSURE_MIN_HPA
+        if not var1:  # avoid exception caused by division by zero
+            raise ArithmeticError(
+                "Invalid result possibly related to error while reading the calibration registers"
+            )
         pressure = 1048576.0 - adc
         pressure = ((pressure - var2 / 4096.0) * 6250.0) / var1
         var1 = self._pressure_calib[8] * pressure * pressure / 2147483648.0
         var2 = pressure * self._pressure_calib[7] / 32768.0
         pressure = pressure + (var1 + var2 + self._pressure_calib[6]) / 16.0
         pressure /= 100
-        if pressure < _BMP280_PRESSURE_MIN_HPA:
-            return _BMP280_PRESSURE_MIN_HPA
-        if pressure > _BMP280_PRESSURE_MAX_HPA:
-            return _BMP280_PRESSURE_MAX_HPA
+
         return pressure
 
     @property
